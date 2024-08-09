ccb48468d9e60e4d873c6b17945ee6cd04750e3f
moulik-1612 <mouliktiwari1612@gmail.com>
2024-08-09 14:28:46 +0530

    Adding products and its responesiveness for tablet

--- app.js
+++ app.js
@@ -48,12 +48,6 @@ document.querySelector('#travel-box').addEventListener('mouseover', ()=> {
     document.querySelector("#beauty-toy-dropdown-2").style.display = 'none';
 })
 
-// document.querySelector('body').addEventListener('mouseover', ()=> {
-//     console.log('hello');
-//     document.querySelector("#fashion-dropdown-1").style.display = 'none';
-//     document.querySelector("#fashion-dropdown-2").style.display = 'none';
-// })
-
 document.querySelector('#beauty-toy-box').addEventListener('mouseover', ()=> {
     document.querySelector("#beauty-toy-dropdown-1").style.display = 'block';
     document.querySelector("#beauty-toy-dropdown-2").style.display = 'block';
@@ -70,4 +64,34 @@ document.querySelector('#two-wheeler-box').addEventListener('mouseover', ()=> {
 
 document.querySelector('#two-wheeler-box').addEventListener('mouseover', ()=> {
     document.querySelector('#two-wheeler-dropdown').style.display = 'block';
+})
+
+let leftBtn = document.querySelector('#btn-left')
+let rightBtn = document.querySelector('#btn-right');
+
+leftBtn.addEventListener('click', ()=> {
+    leftBtn.style.display = 'none';
+    rightBtn.style.display = 'block'
+})
+
+rightBtn.addEventListener('click', ()=> {
+    leftBtn.style.display = 'block';
+    rightBtn.style.display = 'none';
+})
+
+
+window.addEventListener('resize', () => {
+    let screensize = window.innerWidth;
+    if(screensize <= '750'){
+    // console.log('mobile');
+    document.querySelector('#fashion-dropdown').style.display = 'none';
+    document.querySelector('#electronic-dropdown').style.display = 'none';
+    document.querySelector('#home-furniture-dropdown').style.display = 'none';
+    document.querySelector('#beauty-toy-dropdown').style.display = 'none';
+}else{
+    document.querySelector('#fashion-dropdown').style.display = 'flex';
+    document.querySelector('#electronic-dropdown').style.display = 'flex';
+    document.querySelector('#home-furniture-dropdown').style.display = 'flex';
+    document.querySelector('#beauty-toy-dropdown').style.display = 'flex';
+}
 })
